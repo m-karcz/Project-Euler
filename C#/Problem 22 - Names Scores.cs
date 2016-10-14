2f0ed8441308c7ab91bbf5d26daf2acd0ec52ae2
Mariusz Karcz <ujemnypozyton@gmail.com>
2016-10-14 14:37:45 +0200

    fix formatting

--- C#/Problem 22 - Names Scores.cs
+++ C#/Problem 22 - Names Scores.cs
@@ -6,6 +6,13 @@ using System.Text.RegularExpressions;
 
 public class Problem22{
   public static void Main(){
-      Console.WriteLine(File.ReadAllText("txt/Problem22.txt").Split(',').Select(s=>new Regex(@"\w+").Match(s).Value).OrderBy(s=>s).Select((s,i)=>s.ToCharArray().Select(c=>(c-'A'+1)*(i+1)).Sum()).Sum());
+      Console.WriteLine(
+        File.ReadAllText("txt/Problem22.txt")
+            .Split(',')
+            .Select(s=>new Regex(@"\w+").Match(s).Value)
+            .OrderBy(s=>s)
+            .Select((s,i)=>s.ToCharArray()
+                            .Select(c=>(c-'A'+1)*(i+1)).Sum())
+            .Sum());
   }
 }
